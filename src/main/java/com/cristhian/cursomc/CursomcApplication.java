e7d7d181bcf1c0a45192f4bbbe8310f7c17cc989
Cristhian Ebert <cristhianREbert@gmail.com>
2019-08-04 15:33:04 -0300

    Projeto criado

---
+++ src/main/java/com/cristhian/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.cristhian.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
